139afdb5d5c58e4486c56f039575d3af1fda86cb
Marcelina Lima <marcelina.batista@hotmail.com>
2023-05-08 19:03:45 -0300

    Projeto Criado

---
+++ src/main/java/br/com/mbl/wisedelivery/WisedeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.mbl.wisedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisedeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisedeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mbl/wisedelivery/WisedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mbl.wisedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
